37c73e8236d3d2674dfa6679c10206eec270de65
Sveiczer Dávid <22c-sveiczer@IPARI.SULINET.HU>
2026-01-14 11:33:23 +0100

    6

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 try {
     $pdo = new PDO(
         "mysql:host=localhost;dbname=lamp_projektmunka;charset=utf8mb4",
-        "root",   // adatbázis felhasználó
-        "",       // adatbázis jelszó
+        "web",   // adatbázis felhasználó
+        "web",       // adatbázis jelszó
         [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
