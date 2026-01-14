7e211617042d99740dc19bc224ff7e04e7586bb7
Sveiczer Dávid <22c-sveiczer@IPARI.SULINET.HU>
2026-01-14 12:13:24 +0100

    15

--- db.php
+++ db.php
@@ -6,7 +6,6 @@ $dbname = 'vote';
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
-
 if ($conn->connect_error) {
     http_response_code(500);
     echo json_encode(["error" => "Adatbázis kapcsolat sikertelen: " . $conn->connect_error]);
