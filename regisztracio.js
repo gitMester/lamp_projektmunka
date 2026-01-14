2e75fd32a2c9dde9f4cc7ec3488251e2674fc675
Sveiczer Dávid <22c-sveiczer@IPARI.SULINET.HU>
2026-01-14 12:09:41 +0100

    14

--- regisztracio.js
+++ regisztracio.js
@@ -1,26 +1,61 @@
-document.getElementById('regForm').addEventListener('submit', function (e) {
-  e.preventDefault();
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('regForm').addEventListener('submit', async function(e) {
+        e.preventDefault();
+        const adat = {
+            username: document.getElementById('username').value,
+            password: document.getElementById('password').value
+        };
+        
+          console.log(adat.username, adat.password);
 
-  const adat = {
-      username: document.getElementById('username').value,
-      password: document.getElementById('password').value
-  };
+        fetch('regisztracio.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(adat)
+        })
+        .then(response => response.json())
+        .then(valasz => {
+            document.getElementById('uzenet').innerText = valasz.message;
+        })
+        .catch(error => {
+            document.getElementById('uzenet').innerText = 'Hiba történt';
+            console.log(error);
+        });
 
-  console.log(adat.username, adat.password);
-
-  fetch('regisztracio.php', {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(adat)
-  })
-  .then(response => response.json())
-  .then(valasz => {
-      document.getElementById('uzenet').innerText = valasz.message;
-  })
-  .catch(error => {
-      document.getElementById('uzenet').innerText = 'Hiba történt';
-      console.log(error);
-  });
+    });
 });
+
+
+
+
+
+
+
+// document.getElementById('regForm').addEventListener('submit', function (e) {
+//   e.preventDefault();
+
+//   const adat = {
+//       username: document.getElementById('username').value,
+//       password: document.getElementById('password').value
+//   };
+
+//   console.log(adat.username, adat.password);
+
+//   fetch('regisztracio.php', {
+//       method: 'POST',
+//       headers: {
+//           'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(adat)
+//   })
+//   .then(response => response.json())
+//   .then(valasz => {
+//       document.getElementById('uzenet').innerText = valasz.message;
+//   })
+//   .catch(error => {
+//       document.getElementById('uzenet').innerText = 'Hiba történt';
+//       console.log(error);
+//   });
+// });
